7448d2080e0b68b99d7945e43e5e864e5b64eaf3
博文 <boxie5@126.com>
2018-06-14 15:34:46 +0800

    Update OSSObjectOperation.java

--- src/main/java/com/aliyun/oss/internal/OSSObjectOperation.java
+++ src/main/java/com/aliyun/oss/internal/OSSObjectOperation.java
@@ -71,7 +71,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
